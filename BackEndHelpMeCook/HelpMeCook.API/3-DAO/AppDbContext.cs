7ec739ef7e79580b5182c7e0ca52e78a6753097a
Cyril Mary HILAIRE <rilou007@gmail.com>
2024-08-02 11:52:58 -0400

    Comment data seed.

--- BackEndHelpMeCook/HelpMeCook.API/3-DAO/AppDbContext.cs
+++ BackEndHelpMeCook/HelpMeCook.API/3-DAO/AppDbContext.cs
@@ -12,4 +12,17 @@ public class AppDbContext : DbContext {
     public DbSet<Login> Login { get; set;}
     public DbSet<User> User { get; set; }
     public DbSet<Recipe> Recipe { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // modelBuilder.Entity<User>().HasData(
+        //     new User { UserID = 1, FirstName = "John", LastName = "DOE", CreatedDate = DateTime.Now },
+        //     new User { UserID = 2, FirstName = "Albert", LastName = "FAT", CreatedDate = DateTime.Now }
+        // );
+
+        // modelBuilder.Entity<Login>().HasData(
+        //     new Login { LoginID = 1, Username = "jhon.doe", Password = "pass1", UserID = 1 },
+        //     new Login { LoginID = 2, Username = "albert.fat", Password = "pass2", UserID = 2 }
+        // );
+    }
 }
