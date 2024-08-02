c235510302b01add1d54e03c153ab0af490f6e35
Cyril Mary HILAIRE <rilou007@gmail.com>
2024-08-02 14:43:54 -0400

    Username unique and required.

--- BackEndHelpMeCook/HelpMeCook.API/4-Models/Login.cs
+++ BackEndHelpMeCook/HelpMeCook.API/4-Models/Login.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelpMeCook.API.Models;
 
+[Index(nameof(Username), IsUnique = true)]
 public class Login 
 {
     public int LoginID{ get; set;}
+    
+    [Required, System.ComponentModel.Description("The username is required and unique.")]
     public string Username { get; set; } = "";
     public  string Password { get; set; } = "";
     public int UserID { get; set; }
