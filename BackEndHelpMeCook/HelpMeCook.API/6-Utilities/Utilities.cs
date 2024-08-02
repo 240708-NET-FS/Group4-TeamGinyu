0bcd057e58e66deac487178279a3b9e9e4bec7b8
Jonathan DeLeon <jonathan451@revature.net>
2024-08-02 13:20:35 -0400

    Added separate Utilities classes

--- BackEndHelpMeCook/HelpMeCook.API/6-Utilities/Utilities.cs
+++ BackEndHelpMeCook/HelpMeCook.API/6-Utilities/Utilities.cs
@@ -1,6 +1,46 @@
+using HelpMeCook.API.Models;
+
 namespace HelpMeCook.API.Utilities;
 
-public static class Utilities
+public static class UserUtility
 {
-    // Public static methods below.
+    public static User DTOToUser(UserDTO userDTO)
+    {
+        User newUser = new()
+        {
+            FirstName = userDTO.FirstName, 
+            LastName = userDTO.LastName,
+            CratedDate = userDTO.CratedDate
+        };
+        return newUser;
+    }
+}
+
+public static class LoginUtility
+{
+    public static Login DTOToLogin(LoginDTO loginDTO)
+    {
+        Login newLogin = new()
+        {
+            Username = loginDTO.Username, 
+            Password = loginDTO.Password,
+            UserID = loginDTO.UserID
+        };
+        return newLogin;
+    }
+}
+
+public static class RecipeUtility
+{
+    public static Recipe DTOToRecipe(RecipeDTO recipeDTO)
+    {
+        Recipe newRecipe = new()
+        {
+            RecipeName = recipeDTO.RecipeName, 
+            CratedDate = recipeDTO.CratedDate,
+            RecipeNumber = recipeDTO.RecipeNumber,
+            UserID = recipeDTO.UserID
+        };
+        return newRecipe;
+    }
 }
