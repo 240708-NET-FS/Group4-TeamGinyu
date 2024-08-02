25ad91959f170ea4723574ac053c65347bd091b9
Jonathan DeLeon <jonathan451@revature.net>
2024-08-02 13:19:26 -0400

    Added IUserService interfaces - Need to update UserRepo return types

---
+++ BackEndHelpMeCook/HelpMeCook.API/2-Services/UserService.cs
@@ -0,0 +1,44 @@
+using HelpMeCook.API.DAO;
+using HelpMeCook.API.Models;
+using uUtil = HelpMeCook.API.Utilities.UserUtility;
+
+namespace HelpMeCook.API.Services;
+
+public class UserService : IUserService
+{
+    private readonly UserRepo _userRepo;
+    
+    public UserService(UserRepo userRepo)
+    {
+        _userRepo = userRepo;
+    }
+
+    public async Task<User> CreateUser(UserDTO userToCreate)
+    {
+        User newUser = uUtil.DTOToUser(userToCreate);
+        return newUser; //await _userRepo.Create(newUser);
+    }
+
+    public async Task<ICollection<User>> GetAllUsers()
+    {
+        return await _userRepo.GetAll()!;
+    }
+
+    public async Task<User?> GetUserByID(int userID)
+    {
+        if(userID < 1) throw new ArgumentException("Invalid ID");
+        return await _userRepo.GetByID(userID);
+    }
+
+    public async Task<User?> UpdateUser(UserDTO userToUpdate)
+    {
+        User user = uUtil.DTOToUser(userToUpdate);
+        return user; //await _userRepo.Update(user);
+    }
+
+    public async Task<User> DeleteUser(UserDTO userToDelete)
+    {
+        User user = uUtil.DTOToUser(userToDelete);
+        return user; //await _userRepo.Delete(user);
+    }
+}
