22dda61035328efcd28825aa4fc894c234ad7eb4
Cyril Mary HILAIRE <rilou007@gmail.com>
2024-08-05 07:06:36 -0400

    Init One Page.

---
+++ BackEndHelpMeCook/HelpMeCook.FRONTEND/OnePage/js/login.js
@@ -0,0 +1,27 @@
+const btn_login = document.querySelector('#login-button');
+const btn_register = document.querySelector('#register-button');
+const form_element = document.querySelector('#submition-form')
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('Login page loaded');
+})
+
+form_element.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const clicked_element = event.target
+
+    if(clicked_element === btn_login)
+        sendForm('login_endpoint', 'GET?')
+    else if (clicked_element === btn_register)
+        sendForm('register_endpoint', 'POST')
+});
+
+function sendForm(url, method) {
+    fetch(url, {
+        method:method
+    })
+    .then(response => {
+        //if(response.ok) return response.json()
+    })
+}
