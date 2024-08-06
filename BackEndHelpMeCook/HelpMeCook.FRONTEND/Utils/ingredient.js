fe172c76ae27663580868afccdc1b2ab20f52a74
dmo575 <dmo5@protonmail.com>
2024-08-06 17:34:03 -0400

    working on search

--- BackEndHelpMeCook/HelpMeCook.FRONTEND/Utils/ingredient.js
+++ BackEndHelpMeCook/HelpMeCook.FRONTEND/Utils/ingredient.js
@@ -37,23 +37,38 @@ function makeIngredientTag(name) {
     </div>
 */
 
-function makeRecipeBox(recipeName, ingredientNames) {
+function makeRecipeBox(recipeName, ingredientNames, recipeId) {
     
     const recipe_box = document.createElement('div')
+    recipe_box.id = recipeId
     recipe_box.classList.add('box', 'recipe-box')
     const recipe_title = document.createElement('p')
     recipe_title.classList.add('recipe-title')
     const recipe_tags = document.createElement('div')
     recipe_tags.classList.add('recipe-tags')
+    const buttons_div = document.createElement('div')
+    const button_checkout = document.createElement('a')
+    button_checkout.innerText = 'See recipe'
+    button_checkout.classList.add('button')
+    const button_save = document.createElement('btn')
+    button_save.innerText = 'Save recipe'
+    button_save.classList.add('button')
+
+    // Recipe page
+    button_checkout.href = 'https://www.google.com'
+
     
     ingredientNames.forEach(n => {
         recipe_tags.appendChild(makeIngredientTagNonClosable(n))
     });
 
     recipe_title.textContent = recipeName
+    buttons_div.appendChild(button_checkout)
+    buttons_div.appendChild(button_save)
 
     recipe_box.appendChild(recipe_title)
     recipe_box.appendChild(recipe_tags)
+    recipe_box.appendChild(buttons_div)
 
     return recipe_box
 }
